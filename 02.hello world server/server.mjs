d7441e2b4a35846c15f28cb4ff5470c10b744c9e
JERRY <alirrehman938@gmail.com>
2022-09-29 15:31:20 +0500

    herokuserver

--- 02.hello world server/server.mjs
+++ 02.hello world server/server.mjs
@@ -1,11 +1,19 @@
 import  express  from 'express';
+import cors from 'cors';
 const app = express()
-const port = 3000
+app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.send('Hello World!')
 })
-
+app.get('/weatherdata', (req, res) => {
+  res.send({
+    temperature:'30c',
+    windspeed:'50kph',
+    location:'karachi'
+  })
+})
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`hello world server on port ${port}`)
 })
